d17c55f239352c71b5c50c63722220464214473e
Jason McCarver <slam@parasite.cc>
2021-12-01 21:52:15 -0600

    More useless 'cleanup'

--- 2021/day1/day1.c
+++ 2021/day1/day1.c
@@ -7,8 +7,37 @@ typedef struct {
     int *lines;
 } list_t;
 
-void part1(list_t);
-void part2(list_t);
+void part1(list_t input) {
+    int last = 0, nr = -1;
+    for (int i = 0; i < input.len; i++) {
+        nr  += input.lines[i] > last;
+        last = input.lines[i];
+    }
+    printf("%d\n", nr);
+}
+
+void part2(list_t input) {
+    int s1 = 0, s2 = 0, nr = 0;
+    int p1 = input.lines[0];
+    int p2 = input.lines[1];
+
+    for (int i = 0; i < 3; i++) {
+        s1 += input.lines[i];
+        s2 += input.lines[i+1];
+    }
+    for (int i = 4; i < input.len; i++) {
+        nr += s2 > s1;
+
+        s1 += input.lines[i-1] - p1;
+        p1 = p2;
+
+        s2 += input.lines[i] - p2;
+        p2 = input.lines[i-2];
+    }
+    nr += s2 > s1;
+
+    printf("%d\n", nr);
+}
 
 void main() {
     int i;
@@ -24,41 +53,4 @@ void main() {
 
     part1(input);
     part2(input);
-}
-
-void part1(list_t input) {
-    int last = 0;
-    int nr = -1;
-    for (int i = 0; i < input.len; i++) {
-        nr  += input.lines[i] > last;
-        last = input.lines[i];
-    }
-    printf("%d\n", nr);
-}
-
-void part2(list_t input) {
-    int p1, p2;
-    int s1, s2, nr;
-    s1 = s2 = nr = 0;
-
-    p1 = input.lines[0];
-    p2 = input.lines[1];
-
-    for (int i = 0; i < 3; i++) {
-        s1 += input.lines[i];
-        s2 += input.lines[i+1];
-    }
-
-    for (int i = 4; i < input.len; i++) {
-        nr += s2 > s1;
-
-        s1 += input.lines[i-1] - p1;
-        p1 = p2;
-
-        s2 += input.lines[i] - p2;
-        p2 = input.lines[i-2];
-    }
-    nr += s2 > s1;
-
-    printf("%d\n", nr);
 }

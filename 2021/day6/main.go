f782060447cd026bfdb596b68896ec1be9258063
Jason McCarver <slam@parasite.cc>
2021-12-06 15:43:11 -0600

    Add the day count to output

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -8,13 +8,15 @@ import (
 	"strings"
 )
 
+type ages [10]int
+
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 	if !s.Scan() {
 		panic("Invalid input")
 	}
 
-	var fish [10]int // each element is the count of fish at that timer value
+	var fish ages // each element is the count of fish at that timer value
 	for _, n := range strings.Split(s.Text(), ",") {
 		i, err := strconv.Atoi(n)
 		if err != nil {
@@ -27,8 +29,16 @@ func main() {
 	simulate(fish, 256) // part 2
 }
 
-func simulate(fish [10]int, days int) {
-	for ; days > 0; days-- {
+func (fish ages) count() int {
+	nr := 0
+	for i := 0; i < 9; i++ {
+		nr += fish[i]
+	}
+	return nr
+}
+
+func simulate(fish ages, days int) {
+	for d := 1; d <= days; d++ {
 		// All fish at timer 0 will spawn a new fish with its timer=9 (will be corrected to 8 next)
 		fish[9] = fish[0]
 
@@ -40,11 +50,7 @@ func simulate(fish [10]int, days int) {
 		// Finally, all fish that were at timer=0 reset to timer=6 and since they
 		// spawned new fish at timer=8 we can just use the count from timer=8
 		fish[6] += fish[8]
+		//fmt.Printf("%d %d\n", d, fish.count())
 	}
-
-	nr := 0
-	for i := 0; i < 9; i++ {
-		nr += fish[i]
-	}
-	fmt.Println(nr)
+	fmt.Printf("%d %d\n", days, fish.count())
 }

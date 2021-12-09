abf5ca923ada91aaedc960056432f2b847ebb30a
Jason McCarver <slam@parasite.cc>
2021-12-09 14:50:51 -0600

    Day 9 Part 2

--- 2021/day9/main.go
+++ 2021/day9/main.go
@@ -4,12 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
-type heightMap [][]byte
+type location struct {
+	value   byte
+	visited bool
+}
+
+type coordinate struct {
+	r, c int
+}
 
 type game struct {
-	m          heightMap
+	m          [][]location
+	low        []coordinate
+	coords     []coordinate
 	rows, cols int
 }
 
@@ -20,33 +30,104 @@ func main() {
 	for s.Scan() {
 		g.addRow(s.Text())
 	}
-	//fmt.Println(g)
+	// part 1
 	fmt.Println(g.riskLevel())
+
+	// part 2
+	var basins []int
+	for _, c := range g.lowPoints() {
+		basins = append(basins, g.basinAt(c))
+	}
+	if len(basins) < 3 {
+		panic("Invalid input")
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(basins)))
+	num := 1
+	for i := 0; i < 3; i++ {
+		num *= basins[i]
+	}
+	fmt.Println(num)
+}
+
+func (g *game) lowPoints() []coordinate {
+	if g.low != nil {
+		return g.low
+	}
+
+	g.low = make([]coordinate, 0)
+	for _, c := range g.coordinates() {
+		me := g.m[c.r][c.c]
+		func() {
+			for _, n := range g.neighborsOf(c) {
+				if me.value >= g.at(n).value {
+					return
+				}
+			}
+			g.low = append(g.low, c)
+		}()
+	}
+	return g.low
+}
+
+func (g *game) neighborsOf(c coordinate) []coordinate {
+	return []coordinate{
+		coordinate{c.r - 1, c.c},
+		coordinate{c.r + 1, c.c},
+		coordinate{c.r, c.c - 1},
+		coordinate{c.r, c.c + 1},
+	}
+}
+
+func (g *game) coordinates() []coordinate {
+	if g.coords != nil {
+		return g.coords
+	}
+	g.coords = make([]coordinate, 0, (g.rows)*(g.cols))
+	for r := 1; r <= g.rows; r++ {
+		for c := 1; c <= g.cols; c++ {
+			g.coords = append(g.coords, coordinate{r, c})
+		}
+	}
+	return g.coords
 }
 
 func (g *game) riskLevel() int {
 	ret := 0
-	for r := 1; r <= g.rows; r++ {
-		for c := 1; c <= g.cols; c++ {
-			n := g.m[r][c]
-			if n < g.m[r-1][c] && n < g.m[r+1][c] && n < g.m[r][c-1] && n < g.m[r][c+1] {
-				ret += int(n) + 1
-			}
+	for _, c := range g.lowPoints() {
+		ret += int(g.at(c).value) + 1
+	}
+	return ret
+}
+
+// returns the size of the basin starting at c
+// basin size is the number of locations in the basin, including the starting point
+func (g *game) basinAt(c coordinate) int {
+	//fmt.Println("Enter: ", c)
+	me := &g.m[c.r][c.c]
+	me.visited = true
+	ret := 1
+
+	for _, n := range g.neighborsOf(c) {
+		if g.at(n).visited || g.at(n).value < me.value {
+			continue
 		}
+		//fmt.Println("check: ", x)
+		ret += g.basinAt(n)
 	}
 	return ret
 }
 
 func (g *game) addRow(line string) {
 	l := len(line)
+	border := location{10, true}
 
 	if g.rows == 0 {
 		g.cols = l
-		top := make([]byte, l+2)
-		bot := make([]byte, l+2)
+		top := make([]location, l+2)
+		bot := make([]location, l+2)
 		for i := range top {
-			top[i] = 10
-			bot[i] = 10
+			top[i] = border
+			bot[i] = border
 		}
 		g.m = append(g.m, top)
 		g.m = append(g.m, bot)
@@ -58,10 +139,18 @@ func (g *game) addRow(line string) {
 	g.rows++
 
 	g.m = append(g.m, g.m[g.rows])
-	g.m[g.rows] = make([]byte, l+2)
-	g.m[g.rows][0] = 10
-	g.m[g.rows][l+1] = 10
+	g.m[g.rows] = make([]location, l+2)
+	g.m[g.rows][0] = border
+	g.m[g.rows][l+1] = border
 	for c, num := range line {
-		g.m[g.rows][c+1] = byte(num - '0')
+		loc := location{byte(num - '0'), false}
+		if loc.value == 9 {
+			loc.visited = true
+		}
+		g.m[g.rows][c+1] = loc
 	}
+}
+
+func (g *game) at(rc coordinate) *location {
+	return &g.m[rc.r][rc.c]
 }

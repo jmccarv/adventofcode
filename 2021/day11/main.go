7ccc843ee931d1cceda953e4f712fbe004e0bec6
Jason McCarver <slam@parasite.cc>
2021-12-11 15:41:54 -0600

    Day 11 Part 1

---
+++ 2021/day11/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type octopus struct {
+	energy  int
+	flashed bool
+}
+
+type gameGrid [10][10]octopus
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+
+	var grid gameGrid
+	r := 0
+	for s.Scan() {
+		for c, nr := range s.Text() {
+			grid[r][c].energy = int(nr - '0')
+		}
+		r++
+	}
+
+	p1 := 0
+	for i := 0; i < 100; i++ {
+		p1 += grid.step()
+	}
+	fmt.Println(p1)
+}
+
+func (g *gameGrid) step() (flashed int) {
+	for r := 0; r < 10; r++ {
+		for c := 0; c < 10; c++ {
+			g[r][c].energy++
+			if g[r][c].energy > 9 {
+				g.flash(r, c)
+			}
+		}
+	}
+
+	for r := 0; r < 10; r++ {
+		for c := 0; c < 10; c++ {
+			if g[r][c].flashed {
+				flashed++
+				g[r][c].flashed = false
+				g[r][c].energy = 0
+			}
+		}
+	}
+
+	return
+}
+
+func (g *gameGrid) flash(r, c int) {
+	if g[r][c].flashed {
+		return
+	}
+
+	g[r][c].flashed = true
+
+	g.neighbors(r, c, func(r, c int) {
+		if g[r][c].flashed {
+			return
+		}
+
+		g[r][c].energy++
+		if g[r][c].energy > 9 {
+			g.flash(r, c)
+		}
+	})
+}
+
+func (g *gameGrid) neighbors(r, c int, cb func(int, int)) {
+	for dr := -1; dr < 2; dr++ {
+		for dc := -1; dc < 2; dc++ {
+			rr := r + dr
+			cc := c + dc
+			switch {
+			case rr < 0 || rr > 9 || cc < 0 || cc > 9:
+				continue
+			case rr == r && cc == c:
+				continue
+			}
+			cb(rr, cc)
+		}
+	}
+}
+
+func (g gameGrid) String() (ret string) {
+	for _, row := range g {
+		for _, o := range row {
+			f := " "
+			if o.flashed {
+				f = "*"
+			}
+			ret += fmt.Sprintf("%s%2d ", f, o.energy)
+		}
+		ret += "\n"
+	}
+	return
+}

44d8c069e8ff38f0edce3778b4e5ac39e5b6e7b8
Jason McCarver <slam@parasite.cc>
2021-12-16 14:31:41 -0600

    Day 16 Part 2

--- 2021/day16/main.go
+++ 2021/day16/main.go
@@ -13,9 +13,9 @@ type bits struct {
 
 type packet struct {
 	ver int
-	id  int    // 4 == literal, anything else is an op packet
-	val uint64 // if it's a literal value packet
-	//packets []packet // if it's an op packet, it contains other packets
+	id  int      // 4 == literal, anything else is an operator packet
+	val uint64   // if it's a literal value packet
+	sub []packet // operator packets contain sub packets
 }
 
 func main() {
@@ -23,24 +23,82 @@ func main() {
 	if !s.Scan() {
 		panic("Invalid input")
 	}
-	fmt.Println(s.Text())
-	packetData := explode(s.Text())
-	//fmt.Println(packetData)
 
-	p := packetData.getPackets()
-	fmt.Println(p)
-	p1 := 0
-	for _, x := range p {
-		p1 += x.ver
+	// Convert to a more convenient format to parse
+	packetData := explode(s.Text())
+
+	// Now decode all packets
+	packets := packetData.getPackets()
+
+	fmt.Println("Part1", part1(packets))
+	fmt.Println("Part2", packets[0].value())
+}
+
+func part1(packets []packet) (ret int) {
+	for _, p := range packets {
+		ret += p.ver + part1(p.sub)
 	}
-	fmt.Println("Part1", p1)
+	return
+}
+
+func min(a, b uint64) uint64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b uint64) uint64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func tf(b bool) uint64 {
+	if b {
+		return 1
+	}
+	return 0
+}
+
+func (p packet) value() (ret uint64) {
+	switch p.id {
+	case 4: // literal value
+		ret = p.val
+	case 0: // sum all sub packets
+		for _, sub := range p.sub {
+			ret += sub.value()
+		}
+	case 1: // product
+		ret = 1
+		for _, sub := range p.sub {
+			ret *= sub.value()
+		}
+	case 2: // min
+		ret = p.sub[0].value()
+		for _, s := range p.sub[1:] {
+			ret = min(ret, s.value())
+		}
+	case 3: // max
+		ret = p.sub[0].value()
+		for _, s := range p.sub[1:] {
+			ret = max(ret, s.value())
+		}
+	case 5: // greater than
+		ret = tf(p.sub[0].value() > p.sub[1].value())
+	case 6: // less than
+		ret = tf(p.sub[0].value() < p.sub[1].value())
+	case 7:
+		ret = tf(p.sub[0].value() == p.sub[1].value())
+	}
+	return
 }
 
 // each returned byte represents one bit from the incoming hexadecimal string
 // so if the incoming string were 'A3' you'd get:
 // [ 1 0 1 0 0 0 1 1 ]
 func explode(hex string) (ret bits) {
-	//ret = &bits{}
 	for i := 0; i < len(hex); i += 2 {
 		x, err := strconv.ParseUint(hex[i:i+2], 16, 8)
 		if err != nil {
@@ -59,42 +117,34 @@ func explode(hex string) (ret bits) {
 }
 
 func (b *bits) getPackets() (ret []packet) {
-	for {
-		p := b.getNextPacket()
-		if len(p) == 0 {
-			break
-		}
+	p := b.getNextPacket()
+	for len(p) > 0 {
 		ret = append(ret, p...)
+		p = b.getNextPacket()
 	}
-
 	return
 }
 
 func (b *bits) getNextPacket() (ret []packet) {
-	fmt.Println("getNextPacket()", b)
-
-	var p packet
 	if len(b.b) < 8 {
+		// Ignore any trailing zeros in the encoded message
 		return
 	}
-	p.ver = int(b.next(3))
-	p.id = int(b.next(3))
 
+	p := packet{ver: int(b.next(3)), id: int(b.next(3))}
 	switch p.id {
-	case 4:
-		fmt.Println("getNextPacket(): literal", p)
+	case 4: // literal
 		p.val = b.getLiteral()
 		ret = append(ret, p)
-	default:
-		fmt.Println("getNextPacket(): operator", p)
+	default: // operator packet, contains sub packets
+		p.sub = append(p.sub, b.getSubPackets()...)
 		ret = append(ret, p)
-		ret = append(ret, b.getOperator()...)
 	}
-	fmt.Println("getNextPacket()", ret)
-
 	return
 }
 
+// Take the next x bits and convert them to an integer (our return value)
+// Move our bits slice past those x bits
 func (b *bits) next(x int) (ret uint64) {
 	for _, i := range b.b[0:x] {
 		ret = (ret << 1) | uint64(i)
@@ -119,21 +169,19 @@ func (b *bits) getLiteral() (ret uint64) {
 	return
 }
 
-func (b *bits) getOperator() (ret []packet) {
-	// lenght type ID (1 bit) immediately follows packet header
+func (b *bits) getSubPackets() (ret []packet) {
+	// length type ID (1 bit) immediately follows packet header
 	switch b.next(1) {
-	case 0:
-		// next 15 bits are the total length in bits of the sub packets for this operator
+	case 0: // next 15 bits are the total length in bits of the sub packets for this operator
 		splen := int(b.next(15))
 		sub := bits{b: b.b[0:splen]}
-		fmt.Println("getOp() parsing packet(s)", sub)
+		//fmt.Println("getOp() parsing packet(s)", sub)
 		ret = append(ret, sub.getPackets()...)
-		fmt.Println("getOp, back", ret)
+		//fmt.Println("getOp, back", ret)
 		b.next(splen)
-	case 1:
-		// next 11 bits are the number of sub packets contained by this packet
+	case 1: // next 11 bits are the number of sub packets contained by this packet
 		nrSub := int(b.next(11))
-		fmt.Println("getOp() number sub packets", nrSub)
+		//fmt.Println("getOp() number sub packets", nrSub)
 		for i := 0; i < nrSub; i++ {
 			ret = append(ret, b.getNextPacket()...)
 		}
@@ -147,8 +195,4 @@ func (b bits) String() string {
 		ret[i] = 48 + x
 	}
 	return string(ret)
-}
-
-func (p packet) String() string {
-	return fmt.Sprintf("p{ver %d id %d val %d}", p.ver, p.id, p.val)
 }

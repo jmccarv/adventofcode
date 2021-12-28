7137f366e6ae107fa82ac4e968ccfafb3fa6dcaa
Jason McCarver <slam@parasite.cc>
2021-12-28 08:51:21 -0600

    Day 22 Part 2

--- 2021/day22/main.go
+++ 2021/day22/main.go
@@ -9,54 +9,71 @@ import (
 	"strings"
 )
 
-type region struct {
+type cuboid struct {
 	min point
 	max point
-	on  bool
 }
 
-type cuboids []region
+type cuboids []cuboid
 
+type point [3]int // [x,y,z]
+/*
 type point struct {
 	x, y, z int
 }
+*/
 
 func main() {
 	var grid cuboids
 	input := bufio.NewScanner(os.Stdin)
 
 	re := regexp.MustCompile(`[-0-9]+`)
+	//in:
 	for input.Scan() {
 		s := strings.SplitN(input.Text(), " ", 2)
 		m := re.FindAllString(s[1], 6)
 		if len(m) == 6 {
-			r := region{
-				min: point{atoi(m[0]), atoi(m[2]), atoi(m[4])},
-				max: point{atoi(m[1]), atoi(m[3]), atoi(m[5])},
-				on:  s[0] == "on",
+			w := [6]int{atoi(m[0]), atoi(m[1]), atoi(m[2]), atoi(m[3]), atoi(m[4]), atoi(m[5])}
+			r := cuboid{
+				min: point{min(w[0], w[1]), min(w[2], w[3]), min(w[4], w[5])},
+				max: point{max(w[0], w[1]), max(w[2], w[3]), max(w[4], w[5])},
 			}
-			if r.min.x < -50 || r.min.y < -50 || r.min.z < -50 || r.max.x > 50 || r.max.y > 50 || r.max.z > 50 {
-				continue
-			}
-			grid = append(grid, r)
+
+			// part1
 			/*
-				if s[0] == "on" {
-					grid.add(r)
-				} else {
-					grid.sub(r)
+				for i := 0; i < 3; i++ {
+					if r.min[i] < -50 || r.max[i] > 50 {
+						fmt.Println(i, r.min[i], r.max[i], input.Text())
+						continue in
+					}
 				}
 			*/
+
+			switch s[0] {
+			case "on":
+				grid.add(r)
+			case "off":
+				grid.sub(r)
+			}
 		}
 	}
-	fmt.Println(grid)
-	part1(grid)
+	//fmt.Println(grid)
+	fmt.Println(grid.count())
 }
 
-func (cubes *cuboids) add(r region) {
+func (cubes cuboids) count() (ret int) {
+	for _, c := range cubes {
+		ret += (1 + c.max[0] - c.min[0]) * (1 + c.max[1] - c.min[1]) * (1 + c.max[2] - c.min[2])
+	}
+	return
+}
+
+func (cubes *cuboids) add(r cuboid) {
 	*cubes = append(*cubes, cubes.diff(r)...)
+	//cubes.verify()
 }
 
-func (cubes *cuboids) diff(r region) cuboids {
+func (cubes *cuboids) diff(r cuboid) cuboids {
 	newCuboids := cuboids{r}
 
 	for _, c := range *cubes {
@@ -69,50 +86,68 @@ func (cubes *cuboids) diff(r region) cuboids {
 	return newCuboids
 }
 
-func (cubes *cuboids) sub(r region) {
+func (cubes *cuboids) sub(r cuboid) {
 	var newCuboids cuboids
 	for _, c := range *cubes {
 		newCuboids = append(newCuboids, c.sub(r)...)
 	}
 	*cubes = newCuboids
+	cubes.verify()
 	return
 }
 
+func (cubes cuboids) verify() {
+	if len(cubes) < 2 {
+		return
+	}
+	for i, c1 := range cubes[1:] {
+		for _, c2 := range cubes[0:i] {
+			if c1.overlapsWith(c2) {
+				fmt.Println("*** Overlap ***", c1, c2)
+			}
+		}
+	}
+}
+
 // subtract x from r and return zero or more cuboids as a result
-func (r region) sub(x region) cuboids {
-	return []region{}
-}
+func (r cuboid) sub(x cuboid) cuboids {
 
-func part1(cuboids []region) {
-	grid := make(map[point]struct{})
-
-	for _, c := range cuboids {
-		for _, p := range c.points() {
-			if c.on {
-				grid[p] = struct{}{}
-			} else {
-				delete(grid, p)
-			}
-		}
+	// Quick check to see if they overlap, if they don't
+	// there's no need to perform any processing, we just
+	// return the original cuboid
+	//fmt.Println("sub: ", x, "from", r)
+	if !r.overlapsWith(x) {
+		return []cuboid{r}
 	}
-	fmt.Println(len(grid))
-}
 
-func (r region) points() []point {
-	fmt.Println(r, r.count())
-	ret := make([]point, r.count())
-	for x := r.min.x; x <= r.max.x; x++ {
-		for y := r.min.y; y <= r.max.y; y++ {
-			for z := r.min.z; z <= r.max.z; z++ {
-				ret = append(ret, point{x, y, z})
-			}
+	ret := make([]cuboid, 0, 6)
+
+	for i := 0; i < 3; i++ {
+		if x.min[i] >= r.min[i] {
+			nr := r
+			nr.max[i] = x.min[i] - 1
+			ret = append(ret, nr)
+			r.min[i] = x.min[i]
 		}
+		if x.max[i] <= r.max[i] {
+			nr := r
+			nr.min[i] = x.max[i] + 1
+			ret = append(ret, nr)
+			r.max[i] = x.max[i]
+		}
+		//fmt.Println("i", i, "r", r, "ret", ret)
 	}
+
 	return ret
 }
 
-func (r region) count() int {
-	return (r.max.x - r.min.x) * (r.max.y - r.min.y) * (r.max.z - r.min.z)
+func (r cuboid) overlapsWith(x cuboid) bool {
+	for i := 0; i < 3; i++ {
+		if r.max[i] < x.min[i] || r.min[i] > x.max[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func atoi(s string) int {
@@ -126,6 +161,13 @@ func atoi(s string) int {
 
 func min(a, b int) int {
 	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
 		return a
 	}
 	return b
---
+++ 2021/day22/part1.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type region struct {
+	min point
+	max point
+	on  bool
+}
+
+type cuboids []region
+
+type point struct {
+	x, y, z int
+}
+
+func main() {
+	var grid cuboids
+	input := bufio.NewScanner(os.Stdin)
+
+	re := regexp.MustCompile(`[-0-9]+`)
+	for input.Scan() {
+		s := strings.SplitN(input.Text(), " ", 2)
+		m := re.FindAllString(s[1], 6)
+		if len(m) == 6 {
+			r := region{
+				min: point{atoi(m[0]), atoi(m[2]), atoi(m[4])},
+				max: point{atoi(m[1]), atoi(m[3]), atoi(m[5])},
+				on:  s[0] == "on",
+			}
+			if r.min.x < -50 || r.min.y < -50 || r.min.z < -50 || r.max.x > 50 || r.max.y > 50 || r.max.z > 50 {
+				continue
+			}
+			grid = append(grid, r)
+			/*
+				if s[0] == "on" {
+					grid.add(r)
+				} else {
+					grid.sub(r)
+				}
+			*/
+		}
+	}
+	fmt.Println(grid)
+	part1(grid)
+}
+
+func (cubes *cuboids) add(r region) {
+	*cubes = append(*cubes, cubes.diff(r)...)
+}
+
+func (cubes *cuboids) diff(r region) cuboids {
+	newCuboids := cuboids{r}
+
+	for _, c := range *cubes {
+		var nc cuboids
+		for _, x := range newCuboids {
+			nc = append(nc, x.sub(c)...)
+		}
+		newCuboids = nc
+	}
+	return newCuboids
+}
+
+func (cubes *cuboids) sub(r region) {
+	var newCuboids cuboids
+	for _, c := range *cubes {
+		newCuboids = append(newCuboids, c.sub(r)...)
+	}
+	*cubes = newCuboids
+	return
+}
+
+// subtract x from r and return zero or more cuboids as a result
+func (r region) sub(x region) cuboids {
+	return []region{}
+}
+
+func part1(cuboids []region) {
+	grid := make(map[point]struct{})
+
+	for _, c := range cuboids {
+		for _, p := range c.points() {
+			if c.on {
+				grid[p] = struct{}{}
+			} else {
+				delete(grid, p)
+			}
+		}
+	}
+	fmt.Println(len(grid))
+}
+
+func (r region) points() []point {
+	fmt.Println(r, r.count())
+	ret := make([]point, r.count())
+	for x := r.min.x; x <= r.max.x; x++ {
+		for y := r.min.y; y <= r.max.y; y++ {
+			for z := r.min.z; z <= r.max.z; z++ {
+				ret = append(ret, point{x, y, z})
+			}
+		}
+	}
+	return ret
+}
+
+func (r region) count() int {
+	return (r.max.x - r.min.x) * (r.max.y - r.min.y) * (r.max.z - r.min.z)
+}
+
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+		panic("Invalid input")
+	}
+	return i
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}

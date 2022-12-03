60d0e775d2b485e3ea5efb56d75b6de339406aa9
Jason McCarver <slam@parasite.cc>
2022-12-02 19:18:16 -0600

    cleanup

--- 2022/day2/main.pl
+++ 2022/day2/main.pl
@@ -11,24 +11,15 @@ while (<>) {
 }
 
 my %scores = (
-    AX => 3,
-    AY => 6,
-    AZ => 0,
-
-    BX => 0,
-    BY => 3,
-    BZ => 6,
-
-    CX => 6,
-    CY => 0,
-    CZ => 3,
+    AX => 3, AY => 6, AZ => 0,
+    BX => 0, BY => 3, BZ => 6,
+    CX => 6, CY => 0, CZ => 3,
 );
 
 part1(@input);
 part2(@input);
 
 sub part1 {
-    #print Dumper(\@_);
     my $total = 0;
     for (@_) {
         my $me = 3 - (ord('Z') - ord(substr($_, -1, 1)));
@@ -39,18 +30,9 @@ sub part1 {
 
 sub part2 {
     my %xform = (
-        AX => 'AZ',
-        AY => 'AX',
-        AZ => 'AY',
-
-        BX => 'BX',
-        BY => 'BY',
-        BZ => 'BZ',
-
-        CX => 'CY',
-        CY => 'CZ',
-        CZ => 'CX',
+        AX => 'AZ', AY => 'AX', AZ => 'AY',
+        BX => 'BX', BY => 'BY', BZ => 'BZ',
+        CX => 'CY', CY => 'CZ', CZ => 'CX',
     );
-
-    part1(map { $xform{$_} } @_);
+    part1 map { $xform{$_} } @_
 }

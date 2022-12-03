c31c35faf54ca91721918e91518839a9bf7763d9
Jason McCarver <slam@parasite.cc>
2022-12-03 14:24:43 -0600

    A bit cleaner

--- 2022/day3/main.go
+++ 2022/day3/main.go
@@ -6,14 +6,14 @@ import (
 	"os"
 )
 
-type compartment [52]bool // compartment index is the type's priority-1
+type compartment [53]bool // compartment index is the type's priority
 type rucksack [2]compartment
 
-func idx(t byte) byte {
+func priority(t byte) byte {
 	if t > 'Z' {
-		return t - byte('a')
+		return t - byte('a') + 1
 	}
-	return t - byte('A') + 26
+	return t - byte('A') + 26 + 1
 }
 
 func main() {
@@ -24,10 +24,10 @@ func main() {
 		line := s.Text()
 		var sack rucksack
 		for _, t := range []byte(line[:len(line)/2]) {
-			sack[0][idx(t)] = true
+			sack[0][priority(t)] = true
 		}
 		for _, t := range []byte(line[len(line)/2:]) {
-			sack[1][idx(t)] = true
+			sack[1][priority(t)] = true
 		}
 		sacks = append(sacks, sack)
 	}
@@ -38,9 +38,9 @@ func main() {
 func part1(sacks []rucksack) {
 	sum := 0
 	for _, sack := range sacks {
-		for i := 0; i < 52; i++ {
+		for i := 1; i <= 52; i++ {
 			if sack[0][i] && sack[1][i] {
-				sum += i + 1
+				sum += i
 			}
 		}
 	}
@@ -50,17 +50,17 @@ func part1(sacks []rucksack) {
 func part2(sacks []rucksack) {
 	sum := 0
 	for gidx := 0; gidx < len(sacks); gidx += 3 {
-		var types [52]int
+		var types [53]int
 		for _, sack := range sacks[gidx : gidx+3] {
-			for i := 0; i < 52; i++ {
+			for i := 1; i <= 52; i++ {
 				if sack[0][i] || sack[1][i] {
 					types[i]++
 				}
 			}
 		}
-		for i := 0; i < 52; i++ {
+		for i := 1; i <= 52; i++ {
 			if types[i] == 3 {
-				sum += i + 1
+				sum += i
 			}
 		}
 	}

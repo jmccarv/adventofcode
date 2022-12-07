15f0e7a12b85b13e2cfbc9530ffe8b6bc70852b8
Jason McCarver <slam@parasite.cc>
2022-12-06 19:07:08 -0600

    Cleanup?

--- 2022/day6/main.go
+++ 2022/day6/main.go
@@ -15,13 +15,12 @@ func main() {
 }
 
 func check(line string, nrUniq int) int {
-	nrUniq--
 	nr, j := 0, 0
 	t0 := time.Now()
-	for i := nrUniq; i < len(line) && nr == 0; i = j + 1 + nrUniq {
+	for i := nrUniq - 1; i < len(line) && nr == 0; i = j + nrUniq {
 		var seen [123]bool // ASCII 'z' == 122
 		nr = i
-		for j = i; j >= i-nrUniq; j-- {
+		for j = i; j >= i-(nrUniq-1); j-- {
 			if seen[line[j]] {
 				nr = 0
 				break
@@ -30,6 +29,6 @@ func check(line string, nrUniq int) int {
 		}
 	}
 	t1 := time.Now()
-	fmt.Println(nrUniq+1, t1.Sub(t0))
+	fmt.Println(nrUniq, t1.Sub(t0))
 	return nr + 1
 }

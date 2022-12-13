5ae992412a6f63b0aba69f780ae8ae847f5d9070
Jason McCarver <slam@parasite.cc>
2022-12-13 08:32:22 -0600

    Massive speedup using a BFS of our graph starting at the end node, instead of the insanity I was doing before

--- 2022/day12/main.go
+++ 2022/day12/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
-	"runtime"
 	"time"
 
 	"github.com/yourbasic/graph"
@@ -22,7 +20,6 @@ type node struct {
 }
 
 var start, end, nrCols int
-var p2start []int
 var nodes []node
 
 func main() {
@@ -35,11 +32,8 @@ func main() {
 		for c := 0; c < len(s.Text()); c++ {
 			h := s.Text()[c]
 			switch h {
-			case 'a':
-				p2start = append(p2start, nodeNr)
 			case 'S':
 				start = nodeNr
-				p2start = append(p2start, nodeNr)
 				h = 'a'
 			case 'E':
 				end = nodeNr
@@ -53,8 +47,7 @@ func main() {
 
 	g := genGraph(nodes)
 
-	part1(g)
-	part2(g)
+	solve(g)
 	fmt.Println("Total Runtime", time.Now().Sub(t0))
 }
 
@@ -63,12 +56,13 @@ func genGraph(nodes []node) *graph.Mutable {
 
 	// We can move from a node to another where the second
 	// node's height is at most one more than ours
+	// But we're going to be searching from the end to the beginning so reverse the edges
 	add := func(a, b node) {
 		if a.h >= b.h-1 {
-			g.AddCost(a.nr, b.nr, 1)
+			g.AddCost(b.nr, a.nr, 0)
 		}
 		if b.h >= a.h-1 {
-			g.AddCost(b.nr, a.nr, 1)
+			g.AddCost(a.nr, b.nr, 0)
 		}
 	}
 
@@ -85,38 +79,27 @@ func genGraph(nodes []node) *graph.Mutable {
 	return g
 }
 
-func part1(g *graph.Mutable) {
-	t0 := time.Now()
-	path, _ := graph.ShortestPath(g, start, end)
-	fmt.Println("Part 1:", len(path)-1, " in", time.Now().Sub(t0))
-}
+func solve(g *graph.Mutable) {
+	// We do a breadth first search starting at the end
+	// The first 'a' we come to is the shortest path for part 2
+	// When we find the start node, we've got the length for part 1
+	var p1, p2 int
 
-func part2(g *graph.Mutable) {
-	t0 := time.Now()
-	ch := make(chan int)
-	workers := make(chan struct{}, runtime.NumCPU())
-	var res int = math.MaxInt
+	// distance traveled to get to any node from the end node
+	dist := make([]int, g.Order())
 
-	go func() {
-		for _, s := range p2start {
-			workers <- struct{}{}
-			go func(s int) {
-				var res int = math.MaxInt
-				path, _ := graph.ShortestPath(g, s, end)
-				if len(path) > 0 && len(path)-1 < res {
-					res = len(path) - 1
-				}
-				ch <- res
-				<-workers
-			}(s)
+	// v is the node coming from and w is the node we're going to
+	graph.BFS(g, end, func(v, w int, _ int64) {
+		dist[w] = dist[v] + 1
+		if nodes[w].h == 'a' && p2 == 0 {
+			// This is the shortest route from any 'a' to the end node
+			p2 = dist[w]
 		}
-	}()
-
-	for i := 0; i < len(p2start); i++ {
-		r := <-ch
-		if r < res {
-			res = r
+		if w == start {
+			p1 = dist[w]
 		}
-	}
-	fmt.Println("Part 2:", res, " in", time.Now().Sub(t0))
+	})
+
+	fmt.Println("Part 1:", p1)
+	fmt.Println("Part 2:", p2)
 }

7a4d98a4167698caa92acb2b66e0cf38f8f3f447
Jason McCarver <slam@parasite.cc>
2022-12-22 13:02:18 -0600

    Fixup

--- util/simple2d/2d.go
+++ util/simple2d/2d.go
@@ -1,7 +1,9 @@
 package simple2d
 
 import (
-	"github.com/jmccarv/adventofcode/util/math"
+	"math"
+
+	sm "github.com/jmccarv/adventofcode/util/math"
 )
 
 type Point struct {
@@ -17,11 +19,11 @@ func (p Point) Sub(q Point) Point {
 }
 
 func (p Point) Min(q Point) Point {
-	return Point{X: math.Min(p.X, q.X), Y: math.Min(p.Y, q.Y)}
+	return Point{X: sm.Min(p.X, q.X), Y: sm.Min(p.Y, q.Y)}
 }
 
 func (p Point) Max(q Point) Point {
-	return Point{X: math.Max(p.X, q.X), Y: math.Max(p.Y, q.Y)}
+	return Point{X: sm.Max(p.X, q.X), Y: sm.Max(p.Y, q.Y)}
 }
 
 func (p Point) Equals(q Point) bool {
@@ -29,7 +31,18 @@ func (p Point) Equals(q Point) bool {
 }
 
 func (p Point) DirectionTo(q Point) Point {
-	return Point{X: math.Cmp(q.X, p.X), Y: math.Cmp(q.Y, p.Y)}
+	return Point{X: sm.Cmp(q.X, p.X), Y: sm.Cmp(q.Y, p.Y)}
+}
+
+// Manhattan distance
+func (p Point) MhDistance(q Point) int {
+	return sm.Abs(p.X-q.X) + sm.Abs(p.Y-p.Y)
+}
+
+// distance between two points, A & B:
+// AB = √( (x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2 )
+func (p Point) Distance(q Point) float64 {
+	return math.Sqrt(math.Pow(float64(q.X-p.X), 2) + math.Pow(float64(q.Y-q.Y), 2))
 }
 
 type Box struct {

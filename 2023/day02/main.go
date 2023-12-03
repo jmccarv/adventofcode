d26ccae14a10c6ef1809cedb8e069b44a2aa67c4
Jason McCarver <slam@parasite.cc>
2023-12-02 21:02:57 -0600

    Go version now more like perl version - use regex to parse

--- 2023/day02/main.go
+++ 2023/day02/main.go
@@ -4,37 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"regexp"
+	"strconv"
 )
 
 func main() {
 	var p1max map[string]int = map[string]int{"red": 12, "green": 13, "blue": 14}
+	re := regexp.MustCompile(`(\d+)\s(\w+)`)
 	var id, p1, p2 int
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
-		var p2max map[string]int = map[string]int{"red": 0, "green": 0, "blue": 0}
-		game := strings.Split(s.Text(), ":")
-		fmt.Sscanf(game[0], "Game %d", &id)
+		id++
+		mx := make(map[string]int)
+		for _, m := range re.FindAllStringSubmatch(s.Text(), -1) {
+			nr, _ := strconv.Atoi(m[1])
+			mx[m[2]] = max(mx[m[2]], nr)
+			fmt.Println(id, m[1], m[2], mx[m[2]])
+		}
+		p2tmp := 1
 		good := true
-		for _, c := range strings.FieldsFunc(game[1], func(r rune) bool { return r == ',' || r == ';' }) {
-			var nr int
-			var color string
-			fmt.Sscanf(c, "%d %s", &nr, &color)
-			//fmt.Println("Game", id, c, nr, color)
-			p2max[color] = max(p2max[color], nr)
+		for color, nr := range mx {
 			if nr > p1max[color] {
 				good = false
 			}
+			p2tmp *= nr
 		}
-		//fmt.Println(id, good, p2max)
 		if good {
 			p1 += id
 		}
-		pwr := 1
-		for _, v := range p2max {
-			pwr *= v
-		}
-		p2 += pwr
+		p2 += p2tmp
 	}
 	fmt.Println("p1", p1)
 	fmt.Println("p2", p2)

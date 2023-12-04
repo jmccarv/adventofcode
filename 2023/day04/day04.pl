1fe7491e80345d6915aa187bbf64b2412a419cbb
Jason McCarver <slam@parasite.cc>
2023-12-04 11:41:10 -0600

    Day 4, now in Perl! .. why not

---
+++ 2023/day04/day04.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+use v5.10;
+
+while (<>) {
+    $id++; $wins=0;
+    ($winning, $numbers) = map { [split /\s+/] } /:\s+((?:\d+\s+)+)\|\s+((?:\d+\s*)+)$/;
+    %winning = map { $_ => 1 } @$winning;
+    $wins += $winning{$_} for (@$numbers);
+    $p1 += 2**($wins-1) if $wins;
+    $p2 += $copies{$id} + 1;
+    for ($i = $id+1; $i < $id+1+$wins; $i++) { $copies{$i} += $copies{$id}+1 };
+}
+say "Part 1 $p1";
+say "Part 2 $p2";

cbea6d36ce130efeb0b75ac908deb879feb7c40c
Jason McCarver <slam@parasite.cc>
2023-12-04 10:15:31 -0600

    Day 04 Part 2

--- 2023/day04/main.go
+++ 2023/day04/main.go
@@ -11,30 +11,35 @@ import (
 )
 
 func main() {
-	var p1 int
+	var p1, p2 int
 	re := regexp.MustCompile(`:\s+((?:\d+\s+)+)\|\s+((?:\d+\s*)+)$`)
 	s := bufio.NewScanner(os.Stdin)
 
-	//var copies = make(map[int]int)
-	//id := 0
+	var copies = make(map[int]int)
+	id := 0
 	for s.Scan() {
-		//id++
+		id++
 		var winning = make(map[int]struct{})
-		matches := re.FindAllStringSubmatch(s.Text(), -1)
-		for _, nr := range strings.Fields(matches[0][1]) {
+		matches := re.FindStringSubmatch(s.Text())
+		for _, nr := range strings.Fields(matches[1]) {
 			val, _ := strconv.Atoi(nr)
 			winning[val] = struct{}{}
 		}
-		nrWinning := 0
-		for _, nr := range strings.Fields(matches[0][2]) {
+		wins := 0
+		for _, nr := range strings.Fields(matches[2]) {
 			val, _ := strconv.Atoi(nr)
 			if _, ok := winning[val]; ok {
-				nrWinning++
+				wins++
 			}
 		}
-		if nrWinning > 0 {
-			p1 += int(math.Pow(2, float64(nrWinning-1)))
+		p2 += copies[id] + 1
+		if wins > 0 {
+			p1 += int(math.Pow(2, float64(wins-1)))
+			for i := id + 1; i < id+1+wins; i++ {
+				copies[i] += copies[id] + 1
+			}
 		}
 	}
 	fmt.Println("Part 1", p1)
+	fmt.Println("Part 2", p2)
 }

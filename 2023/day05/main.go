9da1615d0250b4781df3f295c2e41d91679cbea7
Jason McCarver <slam@parasite.cc>
2023-12-06 15:24:58 -0600

    Much *much* faster -- split ranges instead of brute forcing it

--- 2023/day05/main.go
+++ 2023/day05/main.go
@@ -7,46 +7,50 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 )
 
+// A range is a half-open interval (like a slice in Go) which includes A, but not B
+type Range struct {
+	A, B int
+}
+
+func (r Range) String() string {
+	return fmt.Sprintf("%d-%d", r.A, r.B)
+}
+
+func (r Range) Len() int {
+	return r.B - r.A
+}
+
+func (r1 Range) Overlaps(r2 Range) bool {
+	if r1.A >= r2.A && r1.A < r2.B {
+		return true
+	}
+	if r1.B > r2.A && r1.B <= r2.B {
+		return true
+	}
+	return false
+}
+
+func (r1 Range) Contains(r2 Range) bool {
+	if r1.A <= r2.A && r1.B >= r2.B {
+		return true
+	}
+	return false
+}
+
 type rangeMap struct {
-	srcStart  []int
-	destStart []int
-	length    []int
+	Range
+	destStart int
 }
 
-func (rm *rangeMap) Add(src, dest, length int) {
-	rm.srcStart = append(rm.srcStart, src)
-	rm.destStart = append(rm.destStart, dest)
-	rm.length = append(rm.length, length)
+func (rm rangeMap) String() string {
+	return fmt.Sprintf("%s->%d", rm.Range, rm.destStart)
 }
 
-func (rm *rangeMap) Len() int {
-	return len(rm.srcStart)
-}
-
-func (rm *rangeMap) String() string {
-	var ret string
-	for i := 0; i < rm.Len(); i++ {
-		ret += fmt.Sprintf("%d %d %d\n", rm.srcStart[i], rm.destStart[i], rm.length[i])
-	}
-	return ret
-}
-
-func (rm *rangeMap) Lookup(src int) int {
-	for i, start := range rm.srcStart {
-		//fmt.Println("lookup", src, i, start, rm.destStart[i], rm.length[i])
-		if start <= src && src < start+rm.length[i] {
-			//fmt.Println("found")
-			return rm.destStart[i] + src - start
-		}
-	}
-	return src
-}
+var levels [][]rangeMap
 
 func main() {
-	var maps []*rangeMap
 	var seeds []int
 	s := bufio.NewScanner(os.Stdin)
 	s.Scan()
@@ -56,60 +60,78 @@ func main() {
 	}
 
 	var src, dest, length int
-	rm := &rangeMap{}
+	var rMaps []rangeMap
 	for s.Scan() {
 		n, err := fmt.Sscan(s.Text(), &dest, &src, &length)
 		if err == nil && n == 3 {
-			rm.Add(src, dest, length)
+			rMaps = append(rMaps, rangeMap{Range{src, src + length}, dest})
 		} else {
-			if rm.Len() > 0 {
-				maps = append(maps, rm)
-				rm = &rangeMap{}
+			if len(rMaps) > 0 {
+				levels = append(levels, rMaps)
+				rMaps = []rangeMap{}
 			}
 		}
 
 	}
-	if rm.Len() > 0 {
-		maps = append(maps, rm)
+	if len(rMaps) > 0 {
+		levels = append(levels, rMaps)
 	}
 
 	// part 1
 	p1 := math.MaxInt
 	for _, seed := range seeds {
-		fmt.Println("seed", seed, mapSeed(seed, maps))
-		p1 = min(p1, mapSeed(seed, maps))
+		//fmt.Println("seed", seed, mapSeed(seed, levels))
+		p1 = min(p1, mapSeed(seed, levels))
 	}
 
-	ch := make(chan int)
-	var wg sync.WaitGroup
-	for i := 0; i < len(seeds); i += 2 {
-		fmt.Println("seed range", seeds[i], seeds[i+1])
-		wg.Add(1)
-		go func(i int) {
-			nr := math.MaxInt
-			for seed := seeds[i]; seed < seeds[i]+seeds[i+1]; seed++ {
-				nr = min(nr, mapSeed(seed, maps))
-			}
-			ch <- nr
-			wg.Done()
-		}(i)
-	}
-	go func() {
-		wg.Wait()
-		close(ch)
-	}()
 	p2 := math.MaxInt
-	for nr := range ch {
-		p2 = min(p2, nr)
+	for i := 0; i < len(seeds); i += 2 {
+		//fmt.Println("seed range", seeds[i], seeds[i+1])
+		p2 = min(p2, mapRange(0, Range{seeds[i], seeds[i] + seeds[i+1]}))
 	}
+	//fmt.Println(maps)
 	fmt.Println("Part 1", p1)
 	fmt.Println("Part 2", p2)
 }
 
-func mapSeed(src int, maps []*rangeMap) int {
+func lookup(maps []rangeMap, src int) int {
 	for _, m := range maps {
-		src = m.Lookup(src)
-		//fmt.Println("got:", i, src)
+		//fmt.Println("lookup", src, i, start, rm.destStart[i], rm.length[i])
+		if src >= m.A && src < m.B {
+			return m.destStart + src - m.A
+		}
 	}
 	return src
+}
+
+func mapSeed(src int, levels [][]rangeMap) int {
+	for _, maps := range levels {
+		src = lookup(maps, src)
+	}
+	return src
+}
+
+func mapRange(lvl int, src Range) int {
+	for _, rm := range levels[lvl] {
+		if rm.Range.Contains(src) {
+			ofs := rm.destStart - rm.A
+			s := Range{src.A + ofs, src.B + ofs}
+			if lvl >= len(levels)-1 {
+				return s.A
+			}
+			return mapRange(lvl+1, s)
+		}
+		if rm.A <= src.A && src.A < rm.B {
+			// split the range into two
+			return min(mapRange(lvl, Range{src.A, rm.B}), mapRange(lvl, Range{rm.B, src.B}))
+		}
+		if src.B > rm.A && src.B <= rm.B {
+			// split
+			return min(mapRange(lvl, Range{src.A, rm.A}), mapRange(lvl, Range{rm.A, src.B}))
+		}
+	}
+	if lvl >= len(levels)-1 {
+		return src.A
+	}
+	return mapRange(lvl+1, src)
 }

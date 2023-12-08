6ca396091c562ee6b4595b3a016b8ef9ac07aceb
Jason McCarver <slam@parasite.cc>
2023-12-08 14:51:44 -0600

    Day 08 Part 2

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -7,45 +7,124 @@ import (
 )
 
 type node struct {
-	n string
-	l string
-	r string
+	loc string
+	l   *node
+	r   *node
 }
+
+var directions string
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 	s.Scan()
-	directions := s.Text()
+	directions = s.Text()
 	s.Scan()
 
-	nodes := make(map[string]node)
+	nodes := make(map[string]*node)
 	for s.Scan() {
 		var loc, l, r string
 		fmt.Sscanf(s.Text(), "%s = (%s %s)", &loc, &l, &r)
-		nodes[loc] = node{loc, l[0:3], r[0:3]}
-	}
-	fmt.Printf("%#v\n", nodes)
-
-	p1 := 0
-	i := 0
-	node := nodes["AAA"]
-	fmt.Printf("Start: %#v\n", node)
-	for {
-		p1++
-		fmt.Printf("%s %c", node.n, directions[i])
-		switch directions[i] {
-		case 'L':
-			fmt.Printf(" -> %s", node.l)
-			node = nodes[node.l]
-		case 'R':
-			fmt.Printf(" -> %s", node.r)
-			node = nodes[node.r]
+		l = l[0:3]
+		r = r[0:3]
+		ln, ok := nodes[l]
+		if !ok {
+			ln = &node{loc: l}
+			nodes[l] = ln
 		}
-		fmt.Printf(" = %#v\n", node)
-		if node.n == "ZZZ" {
+		rn, ok := nodes[r]
+		if !ok {
+			rn = &node{loc: r}
+			nodes[r] = rn
+		}
+		n, ok := nodes[loc]
+		if !ok {
+			n = &node{loc: loc}
+			nodes[loc] = n
+		}
+		n.l, n.r = ln, rn
+	}
+
+	part1(nodes)
+	part2(nodes)
+}
+
+func (n *node) move(direction byte) *node {
+	switch direction {
+	case 'L':
+		return n.l
+	case 'R':
+		return n.r
+	}
+	panic(fmt.Sprintf("Invalid direction '%c'", direction))
+
+}
+
+func part1(nodes map[string]*node) {
+	var res, i int
+	node, ok := nodes["AAA"]
+	if !ok {
+		fmt.Println("Invalid input for part 1; skipping")
+		return
+	}
+	for {
+		res++
+		node = node.move(directions[i])
+		if node.loc == "ZZZ" {
 			break
 		}
 		i = (i + 1) % len(directions)
 	}
-	println("Part1", p1)
+	println("Part1", res)
+}
+
+// gcd and lcd functions stolen from the internets
+// https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func lcm(nums ...int) int {
+	if len(nums) < 2 {
+		return 0
+	}
+	a, b := nums[0], nums[1]
+	nums = nums[2:]
+
+	ret := a * b / gcd(a, b)
+	for _, i := range nums {
+		ret = lcm(ret, i)
+	}
+	return ret
+}
+
+// While based on the description of the problem I don't see how
+// the LCM of each path could be guaranteed to be the anser, it
+// does work for the input set provided so ...
+func part2(nodes map[string]*node) {
+	var counts []int
+
+	count := func(node *node) int {
+		var cnt, i int
+		for {
+			cnt++
+			node = node.move(directions[i])
+			if node.loc[2] == 'Z' {
+				break
+			}
+			i = (i + 1) % len(directions)
+		}
+		return cnt
+	}
+
+	for loc, node := range nodes {
+		if loc[2] == 'A' {
+			counts = append(counts, count(node))
+		}
+	}
+	fmt.Println("Part2", lcm(counts...))
 }
